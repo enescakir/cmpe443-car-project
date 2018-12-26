eb860daad4c3266288b8dabee6fba1e243206fc9
Enes Çakır <enes@cakir.web.tr>
2018-12-26 16:44:17 +0300

    Add serial UART communication

---
+++ Library/Serial.c
@@ -0,0 +1,75 @@
+#include "Serial.h"
+
+char    *serialTransmitData     = 0;
+uint8_t serialTransmitCompleted = 0;
+char    serialReceivedCharacter = 0;
+
+void Serial_Init() {
+	//Change the function of TX and RX pins for UART.
+	uint32_t value1  = Serial_UART_TX_PIN;
+	value1 |= 1 << 0;
+	value1 &= ~((1 << 1) | (1 << 2));
+	Serial_UART_TX_PIN = value1;
+	uint32_t value2 = Serial_UART_RX_PIN;
+	value2 |= 1 << 0;
+	value2 &= ~((1 << 1) | (1 << 2));
+	Serial_UART_RX_PIN = value2;
+
+	//Turn on UART0.
+	PCONP |= (1 << 3);
+
+	//Enable FIFO for UART0.
+	Serial_UART->FCR |= (1 << 0);
+
+	//In order to change the DLM, DLL and FDR values, Write correct code for enabling the access to Divisor Latches.
+	Serial_UART->LCR |= (1 << 7);
+
+	//Write correct DLM, DLL and FDR values for 9600 baudrate
+	Serial_UART->DLM = (0x01 << 0);
+	Serial_UART->DLL = (0x25 << 0);
+	Serial_UART->FDR = ((0x01 << 0) | (0x03 << 4));
+
+	//Write correct code for disabling the access to Divisor Latches.
+	Serial_UART->LCR &= ~(1 << 7);
+
+	//Change LCR register value for 8-bit character transfer, 1 stop bits and Even Parity.
+	Serial_UART->LCR = (3 << 0) | (0 << 2) | (1 << 3) | (1 << 4);
+
+	//Enable the Receive Data Available and THRE Interrupt.
+	Serial_UART->IER |= (1 << 0); // RBR Interrupt Enable.
+	Serial_UART->IER |= (1 << 1); // THRE Interrupt Enable..
+
+	//Enable UART0_IRQn Interrupt.
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	//Set UART0_IRQn Priority to 5.
+	NVIC_SetPriority(UART0_IRQn, 5);
+//	NVIC_ClearPendingIRQ(TIMER2_IRQn);
+
+}
+
+void UART0_IRQHandler() {
+	uint32_t currentInterrupt = ((Serial_UART->IIR & (0x7 << 1)) >> 1);
+
+	//First if statement is for Receive Data Available interrupt.
+	if (currentInterrupt == 0x02) {
+		serialReceivedCharacter = Serial_ReadData();
+	} else if (currentInterrupt == 0x1) {
+		//Second if statement is for THRE interrupt
+		if (*serialTransmitData > 0) {
+			Serial_WriteData(*serialTransmitData++);
+		} else {
+			serialTransmitCompleted = 1;
+		}
+	}
+}
+
+char Serial_ReadData() {
+	return Serial_UART->RBR;
+}
+
+void Serial_WriteData(char data) {
+	serialTransmitCompleted = 0;
+	Serial_UART->THR = data;
+}
+
---
+++ Library/Serial.h
@@ -0,0 +1,71 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include "LPC407x_8x_177x_8x.h"
+
+#include "SystemStructures.h"
+
+#pragma anon_unions
+
+typedef struct {
+	union {
+		volatile uint8_t RBR;
+		volatile uint8_t THR;
+		volatile uint8_t DLL;
+		uint32_t         RESERVED0;
+	};
+	union {
+		volatile uint8_t  DLM;
+		volatile uint32_t IER;
+	};
+	union {
+		volatile uint32_t IIR;
+		volatile uint8_t  FCR;
+	};
+	volatile uint8_t  LCR;
+	uint8_t           RESERVED1[7];
+	volatile uint8_t  LSR;
+	uint8_t           RESERVED2[7];
+	volatile uint8_t  SSCR;
+	uint8_t           RESERVED3[3];
+	volatile uint32_t ACR;
+	volatile uint8_t  ICR;
+	uint8_t           RESERVED4[3];
+	volatile uint8_t  FDR;
+	uint8_t           RESERVED5[7];
+	volatile uint8_t  TER;
+	uint8_t           RESERVED8[27];
+	volatile uint8_t  RS485CTRL;
+	uint8_t           RESERVED9[3];
+	volatile uint8_t  ADRMATCH;
+	uint8_t           RESERVED10[3];
+	volatile uint8_t  RS485DLY;
+	uint8_t           RESERVED11[3];
+	volatile uint8_t  FIFOLVL;
+} UART_TypeDef;
+
+//Write the base address of the UART0.
+#define Serial_UART_BASE    0x4000C000
+#define Serial_UART    ((UART_TypeDef*) Serial_UART_BASE)
+
+//Write the IOCON address of TX Pin
+#define Serial_UART_TX_PIN_ADDRESS    0x4002C008
+#define Serial_UART_TX_PIN    *((volatile uint32_t*)(Serial_UART_TX_PIN_ADDRESS))
+
+//Write the IOCON address of PX Pin
+#define Serial_UART_RX_PIN_ADDRESS    0x4002C00C
+#define Serial_UART_RX_PIN    *((volatile uint32_t*)(Serial_UART_RX_PIN_ADDRESS))
+
+extern char serialReceivedCharacter;
+extern char *serialTransmitData;
+extern uint8_t serialTransmitCompleted;
+
+void Serial_Init(void);
+
+char Serial_ReadData(void);
+
+void Serial_WriteData(char data);
+
+void Serial_Write(char *data);
+
+#endif
